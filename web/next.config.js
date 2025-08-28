6cf8ade8c9588800d036700e435b9794c27c6522
simon-duchastel <41980219+simon-duchastel@users.noreply.github.com>
2025-08-28 11:27:29 -0400

    chore: Run build and tests on PR and main (#25)

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    // Ignore markdown files during webpack processing
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'ignore-loader'
+    })
+    return config
+  },
 }
+
+export default nextConfig
